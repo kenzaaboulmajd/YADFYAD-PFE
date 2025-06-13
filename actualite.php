89bbee610a1da4af8f06030b23e2268fc0618417
kenzaaboulmajd <kenzaaboulmajd8@gmail.com>
2025-06-13 15:26:47 +0100

    actualites posts image

--- actualite.php
+++ actualite.php
@@ -2,83 +2,149 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets/styles/style.css">
-  <link rel="stylesheet" href="actualite.css">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/styles/style.css">
+    <link rel="stylesheet" href="actualite.css">
+    <title>Document</title>
 </head>
 
 <body>
-  <header>
-    <?php require_once "sections/navbar.php";
+    <header>
+        <?php require_once "sections/navbar.php";
     ?>
-  </header>
-  <section>
-    <div class="container">
-      <div class="header-actualite">
-        <div class="links-tous">
-          <ul>
-            <li> <a href="#"> Tous</a></li>
-            <li> <a href="#"> Problèmes</a></li>
-            <li> <a href="#"> Activitès</a></li>
-            <li> <a href="#"> Expériences</a></li>
-          </ul>
-        </div>
-        <div class="recherche">
-          <input type="text" placeholder="Rechercher...">
-
-        </div>
-      </div>
-      <di class="posts">
-        <div class="post">
-          <div class="post-header">
-            <div class="post-icone"></div>
-            <div class="post-header-contenu">
-              <div class="post-type">Problème</div>
-              <div class="post-date">Il y'a 2 jours</div>
-            </div>
-          </div>
-          <div class="post-contenu">
-            <div class="post-titre">Manque de bénévoles pour notre événement environnemental</div>
-            <div class="post-description">Nous organisons un événement de sensibilisation environnementale le mois
-              prochain et nous manquons de
-              bénévoles pour l'organisation et la logistique. Nous avons besoin d'au moins 10 personnes pour assurer
-              le
-              bon déroulement de l'événement.</div>
-            <div class="post-association">
-              <div class="post-association-avatar"></div>
-              <div class="post-association-nom">Association pour le Développement Durable</div>
-            </div>
-            <div class="post-interactions">
-              <div class="post-interaction-element">
-                <div class="icone"><svg width="16" height="16" viewBox="0 0 16 16" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M12.6666 9.33333C13.66 8.36 14.6666 7.19333 14.6666 5.66667C14.6666 4.69421 14.2803 3.76158 13.5927 3.07394C12.9051 2.38631 11.9724 2 11 2C9.82665 2 8.99998 2.33333 7.99998 3.33333C6.99998 2.33333 6.17331 2 4.99998 2C4.02752 2 3.09489 2.38631 2.40725 3.07394C1.71962 3.76158 1.33331 4.69421 1.33331 5.66667C1.33331 7.2 2.33331 8.36667 3.33331 9.33333L7.99998 14L12.6666 9.33333Z"
-                      stroke="#020817" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
+    </header>
+    <section>
+        <div class="container">
+            <div class="header-actualite">
+                <div class="links-tous">
+                    <ul>
+                        <li> <a href="#"> Tous</a></li>
+                        <li> <a href="#"> Problèmes</a></li>
+                        <li> <a href="#"> Activitès</a></li>
+                        <li> <a href="#"> Expériences</a></li>
+                    </ul>
                 </div>
-                <div class="valeur">3</div>
-              </div>
-              <div class="post-interaction-element">
-                <div class="icone"><svg width="17" height="16" viewBox="0 0 17 16" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M14.67 10C14.67 10.3536 14.5295 10.6928 14.2795 10.9428C14.0294 11.1929 13.6903 11.3333 13.3366 11.3333H5.33665L2.66998 14V3.33333C2.66998 2.97971 2.81046 2.64057 3.06051 2.39052C3.31056 2.14048 3.64969 2 4.00332 2H13.3366C13.6903 2 14.0294 2.14048 14.2795 2.39052C14.5295 2.64057 14.67 2.97971 14.67 3.33333V10Z"
-                      stroke="#020817" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </div>
-                <div class="valeur">12</div>
-              </div>
-            </div>
-          </div>
+                <div class="recherche">
+                    <input type="text" placeholder="Rechercher...">
 
+                </div>
+            </div>
+            <di class="posts">
+                <div class="post">
+                    <div class="post-image"></div>
+                    <div class="post-container">
+
+                        <div class="post-header">
+                            <div class="post-icone"></div>
+                            <div class="post-header-contenu">
+                                <div class="post-type">Problème</div>
+                                <div class="post-date">Il y'a 2 jours</div>
+                            </div>
+                        </div>
+                        <div class="post-contenu">
+                            <div class="post-titre">Manque de bénévoles pour notre événement environnemental</div>
+                            <div class="post-description">Nous organisons un événement de sensibilisation
+                                environnementale
+                                le mois
+                                prochain et nous manquons de
+                                bénévoles pour l'organisation et la logistique. Nous avons besoin d'au moins 10
+                                personnes
+                                pour assurer
+                                le
+                                bon déroulement de l'événement.</div>
+                            <div class="post-association">
+                                <div class="post-association-avatar"></div>
+                                <div class="post-association-nom">Association pour le Développement Durable</div>
+                            </div>
+                            <div class="post-interactions">
+                                <div class="post-interaction-element">
+                                    <div class="icone"><svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M12.6666 9.33333C13.66 8.36 14.6666 7.19333 14.6666 5.66667C14.6666 4.69421 14.2803 3.76158 13.5927 3.07394C12.9051 2.38631 11.9724 2 11 2C9.82665 2 8.99998 2.33333 7.99998 3.33333C6.99998 2.33333 6.17331 2 4.99998 2C4.02752 2 3.09489 2.38631 2.40725 3.07394C1.71962 3.76158 1.33331 4.69421 1.33331 5.66667C1.33331 7.2 2.33331 8.36667 3.33331 9.33333L7.99998 14L12.6666 9.33333Z"
+                                                stroke="#020817" stroke-width="1.33333" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                        </svg>
+                                        <div class="valeur">3</div>
+                                    </div>
+                                </div>
+                                <div class="post-interaction-element">
+                                    <div class="icone"><svg width="17" height="16" viewBox="0 0 17 16" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M14.67 10C14.67 10.3536 14.5295 10.6928 14.2795 10.9428C14.0294 11.1929 13.6903 11.3333 13.3366 11.3333H5.33665L2.66998 14V3.33333C2.66998 2.97971 2.81046 2.64057 3.06051 2.39052C3.31056 2.14048 3.64969 2 4.00332 2H13.3366C13.6903 2 14.0294 2.14048 14.2795 2.39052C14.5295 2.64057 14.67 2.97971 14.67 3.33333V10Z"
+                                                stroke="#020817" stroke-width="1.33333" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                        </svg>
+                                    </div>
+                                    <div class="valeur">12</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="post">
+                    <div class="post-image">
+                        <img src="https://placehold.co/600x400" alt="">
+                    </div>
+                    <div class="post-container">
+
+                        <div class="post-header">
+                            <div class="post-icone"></div>
+                            <div class="post-header-contenu">
+                                <div class="post-type">Activité</div>
+                                <div class="post-date">Il y'a 2 jours</div>
+                            </div>
+                        </div>
+                        <div class="post-contenu">
+                            <div class="post-titre">Manque de bénévoles pour notre événement environnemental</div>
+                            <div class="post-description">Nous organisons un événement de sensibilisation
+                                environnementale
+                                le mois
+                                prochain et nous manquons de
+                                bénévoles pour l'organisation et la logistique. Nous avons besoin d'au moins 10
+                                personnes
+                                pour assurer
+                                le
+                                bon déroulement de l'événement.</div>
+                            <div class="post-association">
+                                <div class="post-association-avatar"></div>
+                                <div class="post-association-nom">Association pour le Développement Durable</div>
+                            </div>
+                            <div class="post-interactions">
+                                <div class="post-interaction-element">
+                                    <div class="icone"><svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M12.6666 9.33333C13.66 8.36 14.6666 7.19333 14.6666 5.66667C14.6666 4.69421 14.2803 3.76158 13.5927 3.07394C12.9051 2.38631 11.9724 2 11 2C9.82665 2 8.99998 2.33333 7.99998 3.33333C6.99998 2.33333 6.17331 2 4.99998 2C4.02752 2 3.09489 2.38631 2.40725 3.07394C1.71962 3.76158 1.33331 4.69421 1.33331 5.66667C1.33331 7.2 2.33331 8.36667 3.33331 9.33333L7.99998 14L12.6666 9.33333Z"
+                                                stroke="#020817" stroke-width="1.33333" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                        </svg>
+                                        <div class="valeur">3</div>
+                                    </div>
+                                </div>
+                                <div class="post-interaction-element">
+                                    <div class="icone"><svg width="17" height="16" viewBox="0 0 17 16" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M14.67 10C14.67 10.3536 14.5295 10.6928 14.2795 10.9428C14.0294 11.1929 13.6903 11.3333 13.3366 11.3333H5.33665L2.66998 14V3.33333C2.66998 2.97971 2.81046 2.64057 3.06051 2.39052C3.31056 2.14048 3.64969 2 4.00332 2H13.3366C13.6903 2 14.0294 2.14048 14.2795 2.39052C14.5295 2.64057 14.67 2.97971 14.67 3.33333V10Z"
+                                                stroke="#020817" stroke-width="1.33333" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                        </svg>
+                                    </div>
+                                    <div class="valeur">12</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
         </div>
-    </div>
-    </div>
-  </section>
-  <script src="script.js"></script>
+        </div>
+    </section>
+    <script src="script.js"></script>
 </body>
 
 </html>
